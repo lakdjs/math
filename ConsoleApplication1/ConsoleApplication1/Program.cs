c800d41abebf22a9eb165695534186fc8bbd7b5e
lakdjs <124062365+lakdjs@users.noreply.github.com>
2024-06-24 12:16:28 +0300

    .

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication1
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Random random = new Random();
+      Console.WriteLine("сколько дурачков стоит?");
+      int n = int.Parse(Console.ReadLine());
+
+      int[] monetki = new int[n];
+
+      for (int i = 0; i < monetki.Length; i++)
+      {
+        monetki[i] =  random.Next(0, 30);
+      }
+      
+      for (int i = 0; i < monetki.Length; i++)
+      {
+        Console.Write(monetki[i] + " ");
+      }
+    }
+  }
+}
